aa670cc8c5a04053a6ba73ee3b90ca8b428acc77
Hlynur <hlynurh@gmail.com>
2018-02-19 04:13:56 +0000

    Update read_and_write_many.R

--- read_and_write_many.R
+++ read_and_write_many.R
@@ -3,16 +3,21 @@
 
 
 library(tidyverse)
+library(glue)
+library(here)
+
 # Dummy files to process
 dir.create("./temp/raw", recursive=T)
 dir.create("./temp/clean")
 
-timabil <- 1950:2017
+timespan <- 1950:2017
 
-timabil %>% 
-  map(~data_frame(date = seq.Date(as.Date(paste0(.x, "-01-01")),
-                                 as.Date(paste0(.x, "-12-31")),
-                                 by=1))) %>% 
+#TIDYVERSE... ASSEMBLE!!!
+#To create the dummy data using purrr
+timespan %>% 
+  map(~data_frame(date = seq.Date(as.Date(glue("{.x}-01-01")),
+                                  as.Date(glue("{.x}-12-31")),
+                                  by=1))) %>% 
   map(~mutate(., a = rnorm(length(.$date)),
                  a1 = rnorm(length(.$date)),
                  a2 = rnorm(length(.$date)),
@@ -22,14 +27,15 @@ timabil %>%
                  c = rexp(length(.$date), 5),
                  c1 = rexp(length(.$date), 5),
                  c2 = rexp(length(.$date), 5))) %>% 
-  walk2(.y = timabil, ~write_csv(x = .x, path = paste0("temp/raw/file_", .y, ".csv")))
+  walk2(.y = timespan, ~write_csv(x = .x, path = glue("temp/raw/file_{.y}.csv")))
 
+all_files <- list.files("./temp/raw", pattern="file")
 
-skjalanofn = list.files("./temp/raw", pattern="file")
-
-df_list <- map(.x = skjalanofn, .f = ~read_csv(here::here("temp", "raw", .x)))
-names(df_list) <- skjalanofn
+#To select, gather and then write the new data to multiple .CSVs
+df_list <- map(.x = all_files, .f = ~read_csv(here::here("temp", "raw", .x)))
+names(df_list) <- all_files
 
 df_list %>% 
   map(~select(., date, a, b, c)) %>%
+  map(~gather(., variable, value, -date)) %>%
   walk2(.y = names(.), ~write_csv(x = .x, path = here::here("temp", "clean", .y)))
