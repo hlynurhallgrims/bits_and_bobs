b7c6d73ab4c12aed664a0da3c446b57c53542774
Hlynur <hlynurh@gmail.com>
2018-02-19 10:43:49 +0000

    Update read_and_write_many.R

--- read_and_write_many.R
+++ read_and_write_many.R
@@ -5,32 +5,32 @@ library(tidyverse)
 library(glue)
 library(here)
 
-# Dummy files to process
+# Dummy folders to process
 dir.create("./temp/raw", recursive=T)
 dir.create("./temp/clean")
 
 timespan <- 1950:2017
 
 #TIDYVERSE... ASSEMBLE!!!
-#To create the dummy data using purrr
+#Dummy data created and written to multiple .CSVs
 timespan %>% 
   map(~data_frame(date = seq.Date(as.Date(glue("{.x}-01-01")),
                                   as.Date(glue("{.x}-12-31")),
                                   by=1))) %>% 
-  map(~mutate(., a = rnorm(length(.$date)),
+  map(~mutate(., a  = rnorm(length(.$date)),
                  a1 = rnorm(length(.$date)),
                  a2 = rnorm(length(.$date)),
-                 b = rpois(length(.$date), 10),
+                 b  = rpois(length(.$date), 10),
                  b1 = rpois(length(.$date), 10),
                  b2 = rpois(length(.$date), 10),
-                 c = rexp(length(.$date), 5),
+                 c  = rexp(length(.$date), 5),
                  c1 = rexp(length(.$date), 5),
                  c2 = rexp(length(.$date), 5))) %>% 
   walk2(.y = timespan, ~write_csv(x = .x, path = glue("temp/raw/file_{.y}.csv")))
 
 all_files <- list.files("./temp/raw", pattern="file")
 
-#To select, gather and then write the new data to multiple .CSVs
+#The data is then manipulated to long format, then written to multiple .CSVs
 df_list <- map(.x = all_files, .f = ~read_csv(here::here("temp", "raw", .x)))
 names(df_list) <- all_files
 
